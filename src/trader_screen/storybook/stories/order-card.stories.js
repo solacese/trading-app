b7272dedee956c83127385d344674f73fbb31922
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-03-05 19:40:47 -0500

    Storybook setup, order card and most of desktop GUI mockup done

--- src/trader_screen/storybook/stories/order-card.stories.js
+++ src/trader_screen/storybook/stories/order-card.stories.js
@@ -17,7 +17,7 @@ export function OrderCard() {
 
   return (
     <div
-      style={{ width: "500px", height: "500px" }}
+      style={{ width: "500px" }}
       className="rounded overflow-hidden shadow-md p-4"
     >
       <ul class="flex w-full">
@@ -46,40 +46,66 @@ export function OrderCard() {
           </button>
         </li>
       </ul>
-      <div className="mt-2 w-1/2">
-        <label
-          for="price"
-          class="block text-sm leading-5 font-medium text-gray-700"
-        >
-          Order Type
-        </label>
-        <Select
-          isDisabled
-          defaultValue={{ label: "Market", value: "Market" }}
-        />
-      </div>
-      <div className="mt-2 w-1/2">
-        <label
-          for="price"
-          class="block text-sm leading-5 font-medium text-gray-700"
-        >
-          Price
-        </label>
-        <div
-          class="mt-2 px-2 rounded-md shadow-sm flex items-center"
-          style={{ backgroundColor: "hsl(0,0%,95%)" }}
-        >
-          <span className="text-gray-500 pointer-events-none">$</span>
-          <input
-            disabled
-            id="price"
-            defaultValue={"89.04"}
-            className="ml-2"
-            style={{ backgroundColor: "inherit" }}
-          />
-          <div class="flex flex-grow justify-end text-gray-700">
-            <span>CAD</span>
+      <div className="mt-2 grid grid-cols-2 grid-rows-2 gap-2">
+        <div>
+          <div>
+            <label for="price" class="block text-sm font-medium text-gray-700">
+              Order Type
+            </label>
+            <div className="h-10">
+              <Select
+                isDisabled
+                defaultValue={{ label: "Market", value: "Market" }}
+                styles={{
+                  control: (base, state) => ({
+                    ...base,
+                    height: "2.5rem", // match h-10
+                    "min-height": "2.5rem"
+                  })
+                }}
+              />
+            </div>
           </div>
+        </div>
+        <div>
+          <label
+            for="order-size"
+            class="block text-sm font-medium text-gray-700"
+          >
+            Order Size
+          </label>
+          <div class="h-10 px-2 rounded-md border flex items-center">
+            <input id="order-size" defaultValue={"89.04"} className="ml-2" />
+            <div class="flex flex-grow justify-end text-gray-700">
+              <span>RY</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <label for="price" class="block text-sm font-medium text-gray-700">
+            Price
+          </label>
+          <div
+            class="h-10 px-2 rounded-md border flex items-center"
+            style={{ backgroundColor: "hsl(0,0%,95%)" }}
+          >
+            <span className="text-gray-500 pointer-events-none">$</span>
+            <input
+              disabled
+              id="price"
+              defaultValue={"89.04"}
+              className="ml-2 min-w-0"
+              style={{ backgroundColor: "inherit" }}
+            />
+            <div class="flex justify-end text-gray-700">
+              <span>CAD</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-full items-end">
+          <button class="w-full h-10 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Place Buy Order
+          </button>
         </div>
       </div>
     </div>
